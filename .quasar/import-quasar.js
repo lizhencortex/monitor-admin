d99a25741de9987350e1f237a0e62abbc0f2176a
wang <wanggenwang@nuobei.cn>
2019-02-23 16:15:21 +0800

    自动产生的文件

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,17 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+
+import Vue from 'vue'
+
+import {Quasar,QCard,QCheckbox,QModal,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QTable,QTh,QTr,QTd,Ripple,Notify} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},components: {QCard,QCheckbox,QModal,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QTable,QTh,QTr,QTd},directives: {Ripple},plugins: {Notify} })
